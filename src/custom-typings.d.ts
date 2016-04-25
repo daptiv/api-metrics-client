de57734bddcece6499c16c9cad31e9f0520be98c
James Trinklein <james.trinklein@changepoint.com>
2016-04-25 11:58:10 -0700

    update restify typings

--- src/custom-typings.d.ts
+++ src/custom-typings.d.ts
@@ -1,12 +1,3 @@
 ///<reference path="../typings/main.d.ts"/>
 
-// restify ----
-declare module 'restify' {
-    interface RouteSpec {
-        path: string;
-        name: string;
-        method: string;
-        versions: string[];
-    }
-}
 
