99c1e13e15bd4b373338b37628c27cae0a2556d2
James Trinklein <james.trinklein@changepoint.com>
2016-05-02 17:06:15 -0700

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 var gulp = require('gulp'),
     ts = require('gulp-typescript'),
     tsProj = ts.createProject('tsconfig.json'),
-    jasmine = require('gulp-jasmine');
+    jasmine = require('gulp-jasmine'),
+    plumber = require('gulp-plumber'),
+    tsconfig = require('./tsconfig.json'),
+    outDir = tsconfig.compilerOptions.outDir || 'dist';
+
+function onError(error) {
+    console.log(error);
+}
 
 gulp.task('build', () => {
-    gulp.src('src/**/*.ts')
+
+    return tsProj
+        .src()
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(ts(tsProj))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(outDir));
 });
 
-gulp.task('test', () => {
+gulp.task('test', ['build'], () => {
     gulp.src('dist/**/spec/*.js')
+        .pipe(plumber({ errorHandler: () => {} }))
         .pipe(jasmine());
 });
-
+gulp.task('watch', () => {
+    gulp.start('test');
+    gulp.watch('src/**/*.ts', ['test']);
+});
