d60f3820e8e6b603131dd081a778df4274569e68
greglboxer <greg.boxer@changepoint.com>
2016-07-21 14:38:20 -0700

    Move to restify way of recording endpoint timing

--- src/spec/metrics-logger.ts
+++ src/spec/metrics-logger.ts
@@ -13,13 +13,16 @@ describe('metrics-logger', () => {
     let logger;
     let statsDSpy;
     let metricsKeyBuilder = new MetricsKeyBuilder();
-    let mockRequest = jasmine.createSpyObj('mockRequest', ['']);
-    let mockResponse = jasmine.createSpyObj('mockResponse', ['']);
+    let mockRequest;
+    let mockResponse;
     let mockRoute = <Route>{};
     let routeSpec: RouteSpec;
 
     beforeEach(() => {
         statsDSpy = jasmine.createSpyObj('statsDSpy', ['timing']);
+        mockRequest = jasmine.createSpyObj('mockRequest', ['time']);
+        mockResponse = jasmine.createSpyObj('mockResponse', ['header']);
+
         metricsLogger.registerHandledRouteTimingMetrics(serverSpy, statsDOptions);
         logger = new metricsLogger.MetricsLogFactory(statsDSpy, metricsKeyBuilder).createLogger();
         mockResponse.statusCode = 200;
@@ -37,49 +40,29 @@ describe('metrics-logger', () => {
         expect(serverSpy.on).toHaveBeenCalledWith('after', jasmine.any(Function));
     });
 
-    it('when request timer not available for route should not record time', () => {
-        mockRequest.timers = [];
+    it('when response Response-Time header is available should record time for route', () => {
+        mockResponse.header.and.callFake((headerName) => { return headerName === 'Response-Time' ? 123 : null });
+
+        logger(mockRequest, mockResponse, mockRoute);
+
+        expect(statsDSpy.timing).toHaveBeenCalledWith('tasks.200', 123);
+    });
+
+    it('when response Response-Time header is unavailable should record time for route', () => {
+        spyOn(Date,'now').and.callFake(() => { return 40 });
+        mockRequest.time.and.callFake(() => { return 30 });
+        mockResponse.header.and.callFake((headerName) => { return null });
+
+        logger(mockRequest, mockResponse, mockRoute);
+
+        expect(statsDSpy.timing).toHaveBeenCalledWith('tasks.200', 10);
+    });
+
+    it('when both response Response-Time header and request time is unavailable should not record time', () => {
+        mockResponse.header.and.callFake((headerName) => { return null });
 
         logger(mockRequest, mockResponse, mockRoute);
 
         expect(statsDSpy.timing).not.toHaveBeenCalled();
-    });
-
-    it('when request timer is available for route should record time for route', () => {
-        mockRequest.timers = [{name: 'route-1', time: [0, 10]}, {name: 'route-2', time: [0, 20]}, {name: 'route-3', time: [0, 30]}];
-        mockRoute.name = 'route-2';
-
-        logger(mockRequest, mockResponse, mockRoute);
-
-        expect(statsDSpy.timing).toHaveBeenCalledWith('tasks.200', jasmine.any(Number));
-    });
-
-    describe('metrics timer\'s time', () => {
-        it('should be converted (seconds only) to ms', () => {
-            mockRequest.timers = [{name: 'route-1', time: [5, 0]}];
-            mockRoute.name = 'route-1';
-
-            logger(mockRequest, mockResponse, mockRoute);
-
-            expect(statsDSpy.timing).toHaveBeenCalledWith(jasmine.any(String), 5000);
-        });
-
-        it('should be converted (seconds & ns) to ms', () => {
-            mockRequest.timers = [{name: 'route-1', time: [5, 2000000]}];
-            mockRoute.name = 'route-1';
-
-            logger(mockRequest, mockResponse, mockRoute);
-
-            expect(statsDSpy.timing).toHaveBeenCalledWith(jasmine.any(String), 5002);
-        });
-
-        it('should be converted (ns only) to ms', () => {
-            mockRequest.timers = [{name: 'route-1', time: [0, 1000010]}];
-            mockRoute.name = 'route-1';
-
-            logger(mockRequest, mockResponse, mockRoute);
-
-            expect(statsDSpy.timing).toHaveBeenCalledWith(jasmine.any(String), 1.000010);
-        });
     });
 });
